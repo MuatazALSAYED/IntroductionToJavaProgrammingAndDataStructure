ff00c57d076d152f4ba158cb2e2216883745206c
Motta <muataz.alsayed@outlook.com>
2022-02-20 07:06:18 +0300

    Changes

--- src/chapter1/listing/WelcomeWithThreeMessages.java
+++ src/chapter1/listing/WelcomeWithThreeMessages.java
@@ -3,8 +3,11 @@ package chapter1.listing;
 public class WelcomeWithThreeMessages {
 
     public static void main(String[] args) {
+
         System.out.println("Programming is fun!");
         System.out.println("Fundamentals First");
         System.out.println("Problem Driven");
+
     }
+
 }
